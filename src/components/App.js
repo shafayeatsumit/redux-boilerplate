e8d7787ce316e3db41d8cd72ca13051d9deeeb0d
SHAFAYEAT SUMIT <sumit@finder-lbs.com>
2017-08-14 22:42:06 +0600

    react-redux starter first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
+
+const App = () => (
+  <div>
+    <AddTodo />
+    
+  </div>
+)
+
+export default App
